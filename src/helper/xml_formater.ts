ce62a0bcf32d0dff4d14c3cdce0572d8786ea041
komla <anson.komla@outlook.fr>
2024-11-13 15:45:22 +0000

    feat(xml formater): basic helper

---
+++ src/helper/xml_formater.ts
@@ -0,0 +1,81 @@
+import { parseStringPromise } from 'xml2js'
+
+
+export class XMLFormater {
+
+    /**
+     * Converts an object to its SOAP envelope representation.
+     * @param action The SOAP action of the request
+     * @param body The object to convert to XML
+     * @param envXtras Additional attributes to add to the Envelope tag
+     * @param soapKey The namespace prefix to use for the SOAP tags. Defaults to 'soap'.
+     * @returns The XML string representing the SOAP envelope.
+     */
+    static objectToSoapEnvelop(action: string, body: any, envXtras: string[] = [], soapKey = 'soap'): string {
+        return `<?xml version="1.0" encoding="utf-16"?>
+            <${soapKey}:Envelope xmlns:${soapKey}="http://schemas.xmlsoap.org/soap/envelope" ${envXtras.join(' ')}>
+                <${soapKey}:Header/>
+                <${soapKey}:Body>
+                    <${action}>
+                        ${this.objectToXml(body)}
+                    </${action}>
+                </${soapKey}:Body>
+            </${soapKey}:Envelope>
+        `
+    }
+    
+    /**
+     * This function takes an object and returns its XML representation.
+     * If the value is a primitive type, it is converted to a string.
+     * If the value is an array, each element is converted recursively and the results are concatenated.
+     * If the value is an object, the object is converted recursively and the results are concatenated.
+     * @param obj The object to convert to XML
+     * @returns The XML representation of the object
+     */
+    static objectToXml(obj: any): string {
+        if (typeof obj !== 'object' || obj === null) return String(obj)
+
+        return Object.entries(obj)
+            .map(([key, value]) => {
+                if (Array.isArray(value)) {
+                    return value.map(item => `<${key}>${this.objectToXml(item)}</${key}>`).join('')
+                }
+
+                return `<${key}>${this.objectToXml(value)}</${key}>`
+            })
+        .join('')
+    }
+
+
+    /**
+     * This function takes an XML string and returns its JavaScript representation.
+     * The XML must be in the format of a single root node with children.
+     * The function will return a JavaScript object with the same structure as the XML.
+     * Attributes will be merged with the node's children.
+     * Arrays will only be created if there are multiple children with the same name.
+     * Strings will be trimmed of whitespace.
+     * @param xml The XML string to convert to a JavaScript object
+     * @param options Options to pass to the xml2js parser
+     * @returns The JavaScript object representation of the XML
+     */
+    static async xmlToObject(xml: string, options: ParseOptions = {}): Promise<any> {
+
+        const defaultOptions: ParseOptions = {
+            explicitArray: false,    // Not to create arrays for single elements
+            ignoreAttrs: false,      // Ignore attributes
+            mergeAttrs: true,        // Merge attributes
+            trim: true,              // Trim strings
+            ...options
+        };
+
+        return parseStringPromise(xml, defaultOptions)
+    }
+    
+}
+
+interface ParseOptions {
+    explicitArray?: boolean;
+    ignoreAttrs?: boolean;
+    mergeAttrs?: boolean;
+    trim?: boolean;
+}
---
+++ tests/helper/xml_formater.test.ts
@@ -0,0 +1,79 @@
+import { describe, it, expect } from 'vitest'
+import { XMLFormater } from '#helper/xml_formater'
+
+describe('XML Fiormater tests', () => {
+    it('Should convert an object to XML', () => {
+        const obj = {
+            name: 'John',
+            age: 30,
+            address: {
+                street: '123 Main St',
+                city: 'Anytown',
+                state: 'CA',
+                zip: '12345'
+            }
+        }
+
+        const xml = XMLFormater.objectToXml(obj)
+
+        expect(xml).toMatchInlineSnapshot(`"<name>John</name><age>30</age><address><street>123 Main St</street><city>Anytown</city><state>CA</state><zip>12345</zip></address>"`)
+    })
+
+    it('Should convert an XML string to an object', async () => {
+        const xml = `
+            <person>
+                <name>John</name>
+                <age>30</age>
+                <address>
+                    <street>123 Main St</street>
+                    <city>Anytown</city>
+                    <state>CA</state>
+                    <zip>12345</zip>
+                </address>
+            </person>
+        `
+
+        const expected = {
+            person: {
+                name: 'John',
+                age: '30',
+                address: {
+                    street: '123 Main St',
+                    city: 'Anytown',
+                    state: 'CA',
+                    zip: '12345'
+                }
+            }
+        }
+
+        expect((await XMLFormater.xmlToObject(xml))).toStrictEqual(expected)
+    })
+
+    it('Should convert an object to a SOAP envelope', () => {
+        const obj = {
+            name: 'John',
+            age: 30,
+            address: {
+                street: '123 Main St',
+                city: 'Anytown',
+                state: 'CA',
+                zip: '12345'
+            }
+        }
+
+        const xml = XMLFormater.objectToSoapEnvelop('action', obj)
+
+        expect(xml).toMatchInlineSnapshot(`
+          "<?xml version="1.0" encoding="utf-16"?>
+                      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope" >
+                          <soap:Header/>
+                          <soap:Body>
+                              <action>
+                                  <name>John</name><age>30</age><address><street>123 Main St</street><city>Anytown</city><state>CA</state><zip>12345</zip></address>
+                              </action>
+                          </soap:Body>
+                      </soap:Envelope>
+                  "
+        `)
+    })
+})
