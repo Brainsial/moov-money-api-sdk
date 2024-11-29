8bba9505752b6fe713343bde26045af9740a5019
Gontran <anson.komla@outlook.fr>
2024-11-29 23:32:33 +0000

    chore(server): dynamic port

--- servers/default.server.ts
+++ servers/default.server.ts
@@ -10,15 +10,28 @@ export class DefaultServer {
 
     public start() {
         this._server.listen(this._port, '0.0.0.0')
-        console.log(`Server started on port ${this._port}`);
+
+        this._server.on('error', (err: NodeJS.ErrnoException) => {
+            if (err.code === 'EADDRINUSE') {
+                this._port++
+                this.start()
+            }
+        })
+
+        this._server.on('listening', () => {
+            console.info(`Server started on port ${this._port}`);
+        })
+
+        this._server.on('close', () => {
+            console.info(`Server stopped on port ${this._port}`);
+        })
+
         
         return this
     }
 
     public stop() {
         this._server.close()
-        console.log(`Server stopped on port ${this._port}`);
-        
         return this
     }
 
