16dba7144088b007e4c5ea6988403a746c180681
Gontran <anson.komla@outlook.fr>
2024-11-29 13:32:04 +0000

    chore(appConfig): refactoring

--- src/contract/app_config_contract.ts
+++ src/contract/app_config_contract.ts
@@ -1,7 +1,10 @@
+import { StatusMessages } from "#common/interfaces/status_messages";
+
 export abstract class AppConfigContract {
     abstract get username(): string
     abstract get password(): string
     abstract get baseUrl(): string
+    abstract get language(): keyof StatusMessages
     abstract get encryptionKey(): string
     abstract get requestTimeout(): number
     abstract get isValid(): boolean
@@ -9,6 +12,7 @@ export abstract class AppConfigContract {
     abstract set username(username: string)
     abstract set password(password: string)
     abstract set baseUrl(baseUrl: string)
+    abstract set language(language: keyof StatusMessages)
     abstract set encryptionKey(encryptionKey: string)
     abstract set requestTimeout(requestTimeout: number)
 }
