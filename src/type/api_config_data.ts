823c871e173f44dab8050f47be1148bf2f3f42c3
Gontran <anson.komla@outlook.fr>
2024-11-29 13:33:43 +0000

    chore(apiConfig): refactoring

--- src/type/api_config_data.ts
+++ src/type/api_config_data.ts
@@ -1,21 +1,33 @@
+import { StatusMessages } from "#common/interfaces/status_messages"
+
 export type APIConfigData = {
     /**
      * Your Moov Money API username.
      */
     username: string,
+    
     /**
      * Your Moov Money API password.
      */
     password: string,
+
     /**
      * Your Moov Money API base URL.
      */
     baseUrl: string,
+
+    /**
+     * Your Moov Money API language.
+     * Optional and defaults to 'en'.
+     */
+    language?: keyof StatusMessages,
+
     /**
      * Your Moov Money API encryption key.
      * Optional and defaults to 'tlc12345tlc12345tlc12345tlc12345'.
      */
     encryptionKey?: string,
+
     /**
      * Your Moov Money API request timeout in seconds.
      * Optional and defaults to 60.
